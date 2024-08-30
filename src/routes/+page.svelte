1c0a40910ad1d370399ecb26d0033f3b3b254a12
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-08-30 13:06:38 +0700

    HTML tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,23 @@
 	let name = 'youyoumu';
 	const src =
 		'https://avatars.githubusercontent.com/u/19760884?s=400&u=6762f1be1dfc466fc31654873d46722d40537392&v=4';
+	let string = `asdasd asdasdasd asdasdasd asdasda <strong>HTML!!<strong/>`;
 </script>
 
 <h1>Hello {name.toUpperCase()}!</h1>
 <img {src} alt="profile" />
 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium atque odit ducimus!</p>
 <Nested />
+<p>
+	{string}
+</p>
+<p>
+	{@html string}
+</p>
 
 <style>
 	p {
 		color: red;
-		font-size: 2rem;
+		font-size: 1.5rem;
 	}
 </style>
