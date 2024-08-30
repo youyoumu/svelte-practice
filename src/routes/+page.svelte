a098c3ef8ee6cf830826f990a40e9e62b73b1f75
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-08-30 16:33:58 +0700

    Checkbox inputs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -94,7 +94,22 @@
 
 	let a = 1;
 	let b = 2;
+
+	let yes = false;
 </script>
+
+<label>
+	<input type="checkbox" bind:checked={yes} />
+	Yes! Send me regular email spam
+</label>
+
+{#if yes}
+	<p>Thank you. We will bombard your inbox and sell your personal details.</p>
+{:else}
+	<p>You must opt in to continue. If you're not paying, you're the product.</p>
+{/if}
+
+<button disabled={!yes}>Subscribe</button>
 
 <label>
 	<input type="number" bind:value={a} min="0" max="10" />
